2ab6e746f550a7b618b3020609308bf4070a0a2f
parth-bhadra <63583662+parth-bhadra@users.noreply.github.com>
2022-07-15 15:04:20 +0530

    adding two numbers

--- linkedList.js
+++ linkedList.js
@@ -76,14 +76,67 @@ class LinkedList {
     }
 }
 
-const myLinkedList = new LinkedList(10);
-myLinkedList.append(5);
-myLinkedList.append(16);
-myLinkedList.prepend(7);
-// myLinkedList.remove(2);
-myLinkedList.insert(2, 99);
-myLinkedList.insert(2, 179);
-myLinkedList.insert(0, 190);
-console.log(myLinkedList.printList());
+const l1 = new LinkedList(9);
+l1.append(5);
+l1.append(6);
 
-// console.log(myLinkedList);
+const l2 = new LinkedList(9);
+l2.append(4);
+// l2.append(5);
+
+console.log(l1.printList());
+console.log(l2.printList());
+
+function addTwoNumbers(l1, l2) {
+
+    // prepare the linked lists
+    if (l1.length > l2.length) {
+        let cnt = l1.length - l2.length;
+        while (cnt) {
+            l2.prepend(0);
+            cnt--;
+        }
+    } else if (l2.length > l1.length) {
+        let cnt = l2.length - l1.length;
+        while (cnt) {
+            l1.prepend(0);
+            cnt--;
+        }
+    }
+
+    function recursiveAddition(l1, l2) {
+        // base case
+        if (l1.head.next == null || l2.head.next == null) {
+            let carry = 0;
+            const x = l1 ? l1.head.value : 0;
+            const y = l2 ? l2.head.value : 0;
+            let sum = x + y;
+            carry = Math.floor(sum / 10);
+            const result = new LinkedList(sum % 10);
+            return [result, carry];
+        }
+
+        //recursive case
+        let carry = 0;
+        const x = l1 ? l1.head.value : 0;
+        const y = l2 ? l2.head.value : 0;
+        l1.head = l1.head.next; l1.length --;
+        l2.head = l2.head.next; l2.length --;
+        const resultArray = recursiveAddition(l1, l2);
+        let sum = x + y + resultArray[1];
+        carry = Math.floor(sum / 10);
+        resultArray[0].prepend(sum % 10);
+        resultArray[0].printList();
+        return [resultArray[0], carry];
+    }
+
+    return recursiveAddition(l1,l2);
+
+}
+
+const response = addTwoNumbers(l1,l2);
+if(response[1]) {
+    response[0].prepend(response[1]);
+}
+console.log(response[0].printList());
+
